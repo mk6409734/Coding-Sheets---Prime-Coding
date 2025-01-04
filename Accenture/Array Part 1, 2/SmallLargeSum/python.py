ca19bc01549b7773b5245d73e1e0fbbbe3247d7a
Mohit Gautam <160742153+mk6409734@users.noreply.github.com>
2025-01-04 15:12:32 +0530

    Update python.py
    this solution is good

--- Accenture/Array Part 1, 2/SmallLargeSum/python.py
+++ Accenture/Array Part 1, 2/SmallLargeSum/python.py
@@ -1,43 +1,29 @@
-def SmallLargeSum(array):
-    if len(array) <= 3:
+def SmallLargeSum(arr):
+    if len(arr) <= 3:
         return 0
-
-    even_elements = []
-    odd_elements = []
-
-    # Separate elements based on their positions
-    for i in range(len(array)):
+    even = []
+    odd = []
+    for i in range(len(arr)):
         if i % 2 == 0:
-            even_elements.append(array[i])
-        else:
-            odd_elements.append(array[i])
-
-    # Find second largest in even_elements
-    if len(even_elements) < 2:
+            even.append(arr[i])
+        else: 
+            odd.append(arr[i])
+            
+        
+    even.sort(reverse=True)        
+    odd.sort(reverse=True)   
+    
+    if len(even) < 2 or len(odd) < 2:
         return 0
-    max1_even = float('-inf')
-    max2_even = float('-inf')
-    for num in even_elements:
-        if num > max1_even:
-            max2_even = max1_even
-            max1_even = num
-        elif num > max2_even:
-            max2_even = num
+        
+    second_even = even[1]
+    second_odd = odd[1]
+    
+    return second_even + second_odd
+    
 
-    # Find second largest in odd_elements
-    if len(odd_elements) < 2:
-        return 0
-    max1_odd = float('-inf')
-    max2_odd = float('-inf')
-    for num in odd_elements:
-        if num > max1_odd:
-            max2_odd = max1_odd
-            max1_odd = num
-        elif num > max2_odd:
-            max2_odd = num
 
-    return max2_even + max2_odd
-
-# Sample test cases
-print(SmallLargeSum([3, 2, 1, 7, 5, 4]))  # Output: 7
-print(SmallLargeSum([4, 0, 7, 9, 6, 4, 2]))  # Output: 10
+# Sample Test Case 1 
+print(SmallLargeSum([3, 2, 1, 7, 5, 4])) # Output: 7
+# Sample Test Case 2 
+print(SmallLargeSum([4, 0, 7, 9, 6, 4, 2])) # Output: 10
